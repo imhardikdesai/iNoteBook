74440c68202c5d3627b01f61abe28c873da27da0
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-18 11:29:19 +0530

    add authentaction of multiple users in database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res) => {
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+// Create a user with a POST request on localhost:5000/api/auth/createuser 
+router.post('/createuser', [
+    body('name', "Enter Valid Name").isLength({ min: 3 }),
+    body('email', "Enter Valid Email").isEmail(),
+    body('password', "Enter Valid Password").isLength({ min: 5 })
+], async (req, res) => {
+
+    //if there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        let user = await User.findOne({ email: req.body.email })
+        // Check whether the user with this email exists already   
+        if (user) {
+            return res.status(400).json({ "error": "Sorry a user with this email is already exists" })
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.send(user)
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some Error Occurred")
+    }
 })
 
 module.exports = router;
