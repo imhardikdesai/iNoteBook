89cedebca6e2aa0c206f6401474e7bc802670745
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-27 20:33:55 +0530

    add router to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      Welcome to iNoteBook
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
