a99efff3a884225c5977672c309425116ff8b4ea
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-29 15:23:08 +0530

    add context Api to the project

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NoteState from "./context/notes/NoteState";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,35 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props) => {
+    const noteInitial = [
+        {
+            "_id": "6383395a77f376d8e53f5b04",
+            "user": "638208c9acb3e399a94ed209",
+            "title": "Buy Car",
+            "description": "i want to buy car",
+            "tag": "Shopping",
+            "date": "2022-11-27T10:18:02.751Z",
+            "__v": 0
+        },
+        {
+            "_id": "63835273d88f11bc734480c2",
+            "user": "638208c9acb3e399a94ed209",
+            "title": "Home buy",
+            "description": "i want to buy home",
+            "tag": "Shopping",
+            "date": "2022-11-27T12:05:07.175Z",
+            "__v": 0
+        }
+    ]
+
+    const [notes, setNotes] = useState(noteInitial);
+
+    return (
+        <NoteContext.Provider value={{ notes, setNotes }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState
